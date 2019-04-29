a7f8f1781492f1b0a5d784fb9318622a8d8682cc
Cong Tri <nctri1501@gmail.com>
2019-04-29 13:53:42 +0700

    your msg

---
+++ src/main/java/edu/hcmuaf/tms/controller/HelloController.java
@@ -0,0 +1,20 @@
+package edu.hcmuaf.tms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = { "/", "index.html" }, method = { RequestMethod.GET })
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value = { "/staff/trainer/add" }, method = { RequestMethod.GET })
+	public String addTrainer() {
+		return "add-trainer";
+	}
+	
+}
---
+++ src/test/java/edu/hcmuaf/tms/TrainingManagmentSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.hcmuaf.tms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingManagmentSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
